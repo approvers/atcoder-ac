69ec76cda04f3025eded359677b84bda248d311d
shun-shobon <shun.shobon@gmail.com>
2020-07-05 15:22:24 +0900

    build: add typescript and webpack

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import * as path from "path";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import { ConfigurationFactory } from "webpack";
+
+const config: ConfigurationFactory = () => ({
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: {
+    "content/index": path.join(__dirname, "src", "content", "index.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "static", to: ".", },
+      ],
+    }),
+  ],
+});
+
+export default config;
